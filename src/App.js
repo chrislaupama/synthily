e6ce43fd67e6920c89b5fee3fd9216374d50d714
Chris Laupama <chrislaupama@Chriss-MacBook-Pro.local>
2019-04-02 12:41:58 +1300

    we can play a tone!

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
 import React, { Component } from 'react'
+import Tone from 'tone'
 import './App.css'
 
 class App extends Component {
   render() {
+    //create a synth and connect it to the master output (your speakers)
+    const synth = new Tone.Synth().toMaster()
+
+    //trigger the synth with an on-click
+    const playSynth = () => {
+      //play a middle 'C' for the duration of an 8th note
+      synth.triggerAttackRelease('C4', '8n')
+    }
+
     return (
       <div className='App'>
         <h1>Testing</h1>
+        <button onClick={playSynth}>Play Synth</button>
       </div>
     )
   }
