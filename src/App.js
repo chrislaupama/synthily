0c070d211c973e11de08a1be98c055c49d804cb6
Chris Laupama <chrislaupama@Chriss-MacBook-Pro.local>
2019-04-02 12:29:09 +1300

    removed semi colons

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
-import React, { Component } from 'react';
-import './App.css';
+import React, { Component } from 'react'
+import './App.css'
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-      <h1>Testing</h1>
+      <div className='App'>
+        <h1>Testing</h1>
       </div>
-    );
+    )
   }
 }
 
-export default App;
+export default App
