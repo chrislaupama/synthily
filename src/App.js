a4841db681333bb315ff70e1ece6cf8afa56a7eb
Chris Laupama <chrislaupama@Chriss-MacBook-Pro.local>
2019-04-02 13:39:51 +1300

    added multiple keys to test

--- src/App.js
+++ src/App.js
@@ -3,20 +3,30 @@ import Tone from 'tone'
 import './App.css'
 
 class App extends Component {
+  state = {
+    keys: ['C4', 'D4', 'E4', 'F4', 'G4', 'A4', 'B4']
+  }
+
   render() {
     //create a synth and connect it to the master output (your speakers)
     const synth = new Tone.Synth().toMaster()
 
     //trigger the synth with an on-click
-    const playSynth = () => {
+    const playSynth = e => {
       //play a middle 'C' for the duration of an 8th note
-      synth.triggerAttackRelease('C4', '8n')
+      synth.triggerAttackRelease(e.target.name, '8n')
     }
 
     return (
       <div className='App'>
-        <h1>Testing</h1>
-        <button onClick={playSynth}>Play Synth</button>
+        <h1>Press a key</h1>
+        {this.state.keys.map(keys => {
+          return (
+            <button onClick={playSynth} name={keys}>
+              {keys}
+            </button>
+          )
+        })}
       </div>
     )
   }
